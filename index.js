3e3e147aa0cdfbb389749e980e6f8118d29cf734
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-08-26 23:33:21 -0300

    Create index.js
    Funcionalidade do site.

---
+++ index.js
@@ -0,0 +1,118 @@
+/*function bonecotr() {                 (Serve para quando clicar na imagem exibir um pop up com a frase escolhida)
+    alert("Agente TR com uma MAC-10");  
+}
+
+function bonecoct() {                    
+    alert("Agente CT com uma P90");      
+}
+
+function bonecostrs() {
+    alert("Três agentes terroristas juntos");
+}
+
+function ftcs() {                           
+    alert("Um ct ao lado de um tr");     
+}
+
+function bonecotrak() {                  
+    alert("Agente TR com uma AK-47");     
+}*/                                      
+
+function logo() {
+    alert("Logo do Counter Strike");
+}
+
+function trocarBoneco() {
+    document.getElementById("trocar").src="../imagens/agentect2.png";
+}
+
+function destrocarBoneco() {
+    document.getElementById("trocar").src="../imagens/ctp90.png";
+}
+
+function trocarTr(){
+    document.getElementById("troca").src="../imagens/tr2.png";
+}
+
+function destrocarTr() {
+    document.getElementById("troca").src="../imagens/trmac10.png";
+}
+
+function trocarTrio() {
+    document.getElementById("trisal").src="../imagens/trioct.webp";
+}
+
+function destrocarTrio() {
+    document.getElementById("trisal").src="../imagens/trestrs.webp";
+}
+
+function mudaFoto(foto) {
+    document.getElementById("icone").src = foto;
+}
+
+function mudaMapa() {
+        document.getElementById("fallen").src="../imagensmaps/allmirage.jpg";
+}
+
+function destrocarMapa() {
+    document.getElementById("fallen").src="../imagensmaps/mirage.webp";
+}
+
+function trocarDnv() {
+    document.getElementById("ferGod").src="../imagensmaps/allinferno1.jpeg";
+}
+
+function destrocarDnv() {
+    document.getElementById("ferGod").src="../imagensmaps/inferno.webp"
+}
+
+function trocarDnv1() {
+    document.getElementById("taco").src="../imagensmaps/allnuke.jpg";
+}
+
+function destrocarDnv1() {
+    document.getElementById("taco").src="../imagensmaps/nuke.webp"
+}
+
+function trocarDnv2() {
+    document.getElementById("fnx").src="../imagensmaps/allover.webp";
+}
+
+function destrocarDnv2() {
+    document.getElementById("fnx").src="../imagensmaps/overpass.webp"
+}
+
+function trocarDnv3() {
+    document.getElementById("boltz").src="../imagensmaps/allvertigo1.jpg";
+}
+
+function destrocarDnv3() {
+    document.getElementById("boltz").src="../imagensmaps/vertigo.webp"
+}
+
+function trocarDnv4() {
+    document.getElementById("cold").src="../imagensmaps/allanubis.jpg";
+}
+
+function destrocarDnv4() {
+    document.getElementById("cold").src="../imagensmaps/anubis.webp"
+}
+
+function trocarDnv5() {
+    document.getElementById("gaules").src="../imagensmaps/alldust2.jpg";
+}
+
+function destrocarDnv5() {
+    document.getElementById("gaules").src="../imagensmaps/dust2.webp"
+}
+
+function trocarDnv6() {
+    document.getElementById("nak").src="../imagensmaps/allancient.jpeg";
+}
+
+function destrocarDnv6() {
+    document.getElementById("nak").src="../imagensmaps/ancient.webp"
+}
+
+
+
